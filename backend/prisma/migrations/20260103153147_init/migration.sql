2c90c2c46a4eb5aee19a9ad1df284d1c06ef1099
김하림 <hrkim0529@naver.com>
2026-01-04 00:35:27 +0900

    [feat] schema.prisma 파일의 url 작성 방식 수정 및 migrate 실행

---
+++ backend/prisma/migrations/20260103153147_init/migration.sql
@@ -0,0 +1,80 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "DiffShape" AS ENUM ('CIRCLE', 'RECT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Level" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "leftImageUrl" TEXT NOT NULL,
+    "rightImageUrl" TEXT NOT NULL,
+    "diffCount" INTEGER NOT NULL DEFAULT 0,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Level_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Diff" (
+    "id" TEXT NOT NULL,
+    "levelId" TEXT NOT NULL,
+    "shape" "DiffShape" NOT NULL,
+    "x" INTEGER NOT NULL,
+    "y" INTEGER NOT NULL,
+    "r" INTEGER,
+    "w" INTEGER,
+    "h" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Diff_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attempt" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "levelId" TEXT NOT NULL,
+    "startedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "finishedAt" TIMESTAMP(3),
+    "durationMs" INTEGER,
+    "wrongClicks" INTEGER NOT NULL DEFAULT 0,
+    "foundCount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Attempt_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE INDEX "Diff_levelId_idx" ON "Diff"("levelId");
+
+-- CreateIndex
+CREATE INDEX "Attempt_userId_idx" ON "Attempt"("userId");
+
+-- CreateIndex
+CREATE INDEX "Attempt_levelId_idx" ON "Attempt"("levelId");
+
+-- AddForeignKey
+ALTER TABLE "Diff" ADD CONSTRAINT "Diff_levelId_fkey" FOREIGN KEY ("levelId") REFERENCES "Level"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attempt" ADD CONSTRAINT "Attempt_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attempt" ADD CONSTRAINT "Attempt_levelId_fkey" FOREIGN KEY ("levelId") REFERENCES "Level"("id") ON DELETE CASCADE ON UPDATE CASCADE;

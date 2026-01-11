7b89e702852bb12070169ee44989d1979483ea10
김하림 <hrkim0529@naver.com>
2026-01-11 23:23:04 +0900

    chore: 백엔드 api 수정

--- backend/src/routes/levels.public.ts
+++ backend/src/routes/levels.public.ts
@@ -35,7 +35,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
 
-    const level = await prisma.level.findMany({
+    const level = await prisma.level.findFirst({
         where: { id, isPublished: true },
         select: {
             id: true,
